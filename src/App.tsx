7f1a08857b98bfbde86da1a1b1ab6e418d860eb6
azhechkova <alinazhech@gmail.com>
2023-02-02 14:42:01 +0200

    Setup react project with typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <header className="App-header" />
+    </div>
+  );
+};
+
+export default App;
